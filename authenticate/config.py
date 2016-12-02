e70409e3d165f9da640877633e48e34beb4c6b2a
Jack Serrino <Jserrino@gmail.com>
2016-12-02 00:30:28 -0500

    Force clients to present certificate

--- authenticate/config.py
+++ authenticate/config.py
@@ -8,8 +8,10 @@ class Config(object):
 
 class ProductionConfig(Config):
   SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', None)
+  SECRET_KEY = os.getenv('SECRET_KEY', None)
 
 class DevConfig(Config):
   DEBUG = True
   SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.dirname(__file__), '..', 'dev.db')
   SQLALCHEMY_TRACK_MODIFICATIONS = True
+  SECRET_KEY = 'this-is-a-super-secret-key'
--- authenticate/models.py
+++ authenticate/models.py
@@ -6,6 +6,7 @@ class Application(db.Model):
   __tablename__ = 'applications'
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(80), unique=True)
+  key = db.Column(db.Text)
   creator = db.Column(db.String(80))
   owner = db.Column(db.String(80))
   create_date = db.Column(db.DateTime, default=db.func.now())
@@ -19,7 +20,7 @@ class AccessLog(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   application_id = db.Column(db.Integer, db.ForeignKey('applications.id'))
   time = db.Column(db.DateTime, default=db.func.now())
-  certificate_info = db.Column(db.Text, nullable=True)
+  certificate_info = db.Column(db.Text)
 
   application = db.relationship('Application', back_populates='access_logs')
 
